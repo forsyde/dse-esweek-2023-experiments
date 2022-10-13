d66c8030b44696e638f088a639611502a8d80d80
jordao <jordao@kth.se>
2022-10-13 15:53:17 +0200

    started adding some plots

--- process_data.py
+++ process_data.py
@@ -11,9 +11,9 @@ quantiles = [0.2, 0.5, 0.8]
 
 ## global style configuration
 mpl.rcParams.update({
-    "text.usetex": False,
-    # "font.family": "Computer Modern Roman",
-    'font.size': 11
+    "text.usetex": True,
+    "font.family": "Computer Modern Roman",
+    'font.size': 10
 })
 
 idesyde_data = pd.read_csv('idesyde_benchmark.csv')
@@ -29,6 +29,15 @@ desyde_min_actors = desyde_data[' actors'].min()
 desyde_max_actors = desyde_data[' actors'].max()
 desyde_min_runtime_in_secs = desyde_data[' runtime'].min() / 1000
 desyde_max_runtime_in_secs = desyde_data[' runtime'].max() / 1000
+
+idesyde_quantiles = idesyde_agg.quantile(quantiles, numeric_only=True)
+idesyde_min_plat = idesyde_data['plat'].min()
+idesyde_max_plat = idesyde_data['plat'].max()
+idesyde_min_actors = idesyde_data[' actors'].min()
+idesyde_max_actors = idesyde_data[' actors'].max()
+idesyde_min_runtime_in_secs = idesyde_data[' runtime'].min() / 1000
+idesyde_max_runtime_in_secs = idesyde_data[' runtime'].max() / 1000
+
 
 # print(desyde_agg.quantile([0.2, 0.5, 0.8]))
 # print(idesyde_agg.quantile([0.2, 0.5, 0.8]))
@@ -52,3 +61,23 @@ for plat in range(desyde_min_plat, desyde_max_plat + 1):
 # save the plot
 plt.tight_layout()
 fig.savefig('desyde_benchmark_plot.pdf', transparent=True, bbox_inches="tight")
+
+# first for idesyde
+fig, ax = plt.subplots(1, 1, figsize=(img_width_in_inches, img_height_in_inches))
+for plat in range(idesyde_min_plat, idesyde_max_plat + 1):
+    # for median
+    series_top = idesyde_quantiles.loc[plat, :, quantiles[2]]
+    series_median = idesyde_quantiles.loc[plat, :, quantiles[1]]
+    series_bottom = idesyde_quantiles.loc[plat, :, quantiles[0]]
+    ax.plot(series_median.index, series_median[' runtime'] / 1000.0, linestyle='--', marker='.', linewidth=0.8)
+    ax.set_xlim(idesyde_min_actors, idesyde_max_actors)
+    ax.set_ylim(idesyde_min_runtime_in_secs, idesyde_max_runtime_in_secs)
+    # ax.set_yticks(range(0, ymax + 1))
+    ax.set_xticks(range(idesyde_min_actors, idesyde_max_actors + 1))
+    ax.grid(True, axis='y')
+    ax.set_ylabel('Runtime [s]')
+    ax.set_xlabel('Number of Actors')
+# ax[-1].set_xlabel('Publication Year')
+# save the plot
+plt.tight_layout()
+fig.savefig('idesyde_benchmark_plot.pdf', transparent=True, bbox_inches="tight")
